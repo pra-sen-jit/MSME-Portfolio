8c617f63821c3ffea21bbd91c4c6b7a04ab6d0af
Prasenjit Datta <pr.datta.2021@gmail.com>
2025-04-25 05:06:17 +0530

    fixing navbar 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 "use client";
 import React from "react";
 import LandingPage from "./components/LandingPage";
+import Navbar from "./components/header/Header";
+import Footer from "./components/header/Footer";
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Navbar />
       <LandingPage />
-    </div>
+      <Footer />
+    </>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
 import ProductsPage from "./components/product/ProductsPage.jsx";
-import Footer from "./components/header/Footer.jsx";
 import Explore from "./components/about/Explore.jsx";
-import Header from "./components/header/Header.jsx";
 import EmployeeDashboard from "./components/artisan/EmployeeDashboard.jsx";
 import ContactForm from "./components/ContactForm.jsx";
+import NotFound from "./components/NotFound.jsx";
 
 const router = createBrowserRouter([
   {
@@ -19,14 +18,15 @@ const router = createBrowserRouter([
       { path: "product", element: <ProductsPage /> },
       { path: "artisan", element: <EmployeeDashboard /> },
       { path: "contact", element: <ContactForm /> },
+      // { path: "*", element: <NotFound /> },
     ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Header />
+    {/* <Header /> */}
     <RouterProvider router={router} />
-    <Footer />
+    {/* <Footer /> */}
   </StrictMode>
 );
