a5257c25446c7c3a3cfde9f0a1fadc589858f514
D-roy-2003 <debangshuroy2021@gmail.com>
2025-04-26 18:11:48 +0530

    m

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 "use client";
 import React from "react";
+import { Outlet } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import Navbar from "./components/header/Header";
+import Footer from "./components/header/Footer";
 
 const App = () => {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
   );
 };
 
